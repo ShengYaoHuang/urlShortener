dbc4f79971226938f4ec72e13c04995e42905ba9
Yao <capslock0921@gmail.com>
2020-08-09 22:23:42 +0800

    set mongoose & routes

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
+const methodOverride = require('method-override')
+const routes = require('./routes')
+require('./config/mongoose')
+const PORT = process.env.PORT || 3000
 const app = express()
 
-app.get('/', (req, res) => {
-  res.send('helloooooooo')
-})
+app.engine('handlebars', exphbs({ defaultLayout: 'main', extname: 'handlebars' }))
+app.set('view engine', 'handlebars')
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
 
+app.use(routes)
 
-
-app.listen(3000, () => {
-  console.log('App is running on http://localhost:3000')
+app.listen(PORT, () => {
+  console.log(`App is running on http://localhost:${PORT}`)
 })
